99bf7debf4ff5e3406c2ddaba3175045026bbc10
Matteo Magni <matteo@magni.me>
2013-11-12 22:50:11 +0100

    controller news

---
+++ app/scripts/controllers/news.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('APPersicetoApp')
+  .controller('NewsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
